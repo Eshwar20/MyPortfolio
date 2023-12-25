d0f52042e570d345d35d8624a9f7c74968279c0f
Eshwar20 <saieshwar.97a@gmail.com>
2023-12-25 14:31:47 -0600

    Developed projects and contact sections

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function toggleMenu()
 
 }
 
-document.getElementById('expandLink').addEventListener('click', function () {
-    var expandedContent = document.getElementById('expandedContent');
-    expandedContent.style.display = (expandedContent.style.display === 'none') ? 'block' : 'none';
-});
+// document.getElementById('expandLink').addEventListener('click', function () {
+//     var expandedContent = document.getElementById('expandedContent');
+//     expandedContent.style.display = (expandedContent.style.display === 'none') ? 'block' : 'none';
+// });
+
+function showParagraph(projectId) {
+    // Hide all paragraphs
+    var paragraphs = document.querySelectorAll('.hidden');
+    paragraphs.forEach(function(paragraph) {
+        paragraph.style.display = 'none';
+    });
+
+    // Show the selected paragraph
+    var selectedParagraph = document.getElementById(projectId);
+    selectedParagraph.style.display = 'block';
+}
